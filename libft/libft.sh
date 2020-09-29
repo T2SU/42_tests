d6536401f32f71dfc08b2f1b7b0bad6f804df052
T2SU <seria0no@gmail.com>
2020-09-30 01:12:16 +0900

    메인 스크립트 작성

---
+++ libft/libft.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# 42 libft 테스트 셸 스크립트
+#
+#   Created by : smun (smun@student.42seoul.kr)
+#
+#   First Date : 2020-09-28 16:40
+#
+
+echo "                                               "
+echo "  ___       ___  ________  ________ _________   "
+echo " |\  \     |\  \|\   __  \|\  _____\\___   ___\ "
+echo " \ \  \    \ \  \ \  \|\ /\ \  \__/\|___ \  \_| "
+echo "  \ \  \    \ \  \ \   __  \ \   __\    \ \  \  "
+echo "   \ \  \____\ \  \ \  \|\  \ \  \_|     \ \  \ "
+echo "    \ \_______\ \__\ \_______\ \__\       \ \__\\"
+echo "     \|_______|\|__|\|_______|\|__|        \|__|"
+echo "                                               "
+echo "                                               "
+echo "  42 libft self test script"
+echo
+echo "       created by smun <smun@student.42seoul.kr>"
+echo
+echo
+
+
---
+++ test.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# 42 과제 테스트 메인 셸 스크립트
+#
+#   Created by : smun (smun@student.42seoul.kr)
+#
+#   First Date : 2020-09-28 16:22
+#
+
+export MODE="$1"
+export ROOT=$(pwd)
+
+# config.conf 파일을 읽어 TARGET 변수에 저장. 읽어올 수 없는 경우라면 실패.
+get_directory()
+{
+	# 매개변수로 전달받은 모드가 없을 경우
+	#  = 메인 셸 스크립트에 매개변수를 전달하지 않았을 경우.
+	if [ -z $1 ]
+	then
+		echo "Require a mode name"
+		exit 1
+	fi
+
+	# config.conf 파일이 없을 경우
+	if [ ! -f "config.conf" ]
+	then
+		echo "Please set a config file. (config.conf)"
+		exit 1
+	fi
+
+	# config.conf 파일을 읽은 후 매개변수로 전달된 모드를 검색하여 경로 추출.
+	# 그런 다음 TARGET 매개변수에 정의.
+	export TARGET=$(cat config.conf | grep -oE "$1\=.*" | sed "s/$1=//g")
+}
+
+# 읽어온 TARGET 변수의 디렉토리가 올바른 디렉토리 및 존재하는 디렉토리인지 확인.
+validate_directory()
+{
+	# 대상 디렉토리를 읽어오지 못했을 경우. (config.conf에 없을 경우?)
+	if [ -z $TARGET ]
+	then
+		echo "Unknown target path. (mode: $MODE)"
+		exit 1
+	fi
+
+	local _TARGET=$(eval echo $TARGET)
+	# 대상 디렉토리가 존재하지 않는 디렉토리 일 경우.
+	if [ ! -d $_TARGET ]
+	then
+		echo "Invalid target directory: '$TARGET'. (mode: $MODE)"
+		exit 1
+	fi
+	export TARGET=$_TARGET
+}
+
+# Disclaimer
+disclaimer()
+{
+	echo
+	echo "       <<< Disclaimer >>>"
+	echo
+	echo " * Your use of this script and your reliance on"
+	echo "   any result is solely at your own risk. "
+	echo "   Before run the script, please confirm "
+	echo "   carefully the its sources."
+	echo
+    read -p "continue? (y/n): " ANSWER
+    case $ANSWER in
+        [Yy]* ) break;;
+        * ) exit;;
+    esac
+}
+
+get_directory $MODE
+validate_directory $MODE
+cd $TARGET
+
+echo "Welcome to 42 test scripts."
+
+if [[ $MODE == "libft" ]]
+then
+	disclaimer
+	echo "Libft directory: $TARGET";
+	$ROOT/libft/libft.sh
+fi
+
